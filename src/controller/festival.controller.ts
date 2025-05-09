9b271d1df09d2176767f267a2108a706e10c8e67
Batou <m.schoepper@yahoo.de>
2025-05-09 21:41:58 +0200

    #13 moved the validation function in a separate file for separation of concerns and better testing

--- src/controller/festival.controller.ts
+++ src/controller/festival.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import { FestivalService } from "../services/festival.service";
-import { CustomError, InvalidFestivalId, InvalidUpdateInput } from "../errors/custom-errors";
-import { Festival } from "../entities/festival.entity";
+import { CustomError, InvalidFestivalId } from "../errors/custom-errors";
 import { FestivalUpdateDTO } from "../interfaces/festivalUpdateDTO.interface";
+import { validateFestivalUpdateInput } from "../utils/validation.utils";
 
 export class FestivalController {
   constructor(private readonly festivalService: FestivalService) {
@@ -150,50 +150,4 @@ export class FestivalController {
       }
     }
   }
-}
-
-// function validates the input data you receive and creates an Partial<Festival> object
-// every field is tested for typeof "string" and if the string only consists of whitespace characters
-// mandatory fields throw errors if they're empty
-function validateFestivalUpdateInput(input: FestivalUpdateDTO): Partial<Festival> {
-  const validatedData: Partial<Festival> = {};
-
-  if (typeof input.name === "string" && input.name.trim() !== "") {
-    validatedData.name = input.name.trim();
-  } else if (input.name !== undefined) {
-    throw new InvalidUpdateInput("Invalid name provided");
-  }
-
-  if (typeof input.date === "string" && input.date.trim() !== "") {
-    // date-string is parsed to Date to validate correct ISO-8601 format
-    const parsedDate = Date.parse(input.date.trim());
-    if (isNaN(parsedDate)) {
-      throw new InvalidUpdateInput("Invalid ISO-8601 format for date field");
-    }
-
-    validatedData.date = input.date.trim();
-  } else if (input.date !== undefined) {
-    throw new InvalidUpdateInput("Invalid date provided (ISO-8601 format needed)");
-  }
-
-  if (typeof input.location === "string" && input.location.trim() !== "") {
-    validatedData.location = input.location.trim();
-  } else if (input.location !== undefined) {
-    throw new InvalidUpdateInput("Invalid location provided");
-  }
-
-  if (typeof input.description === "string") {
-    validatedData.description = input.description.trim();
-  }
-
-  if (typeof input.imageUrl === "string") {
-    validatedData.imageUrl = input.imageUrl.trim();
-  }
-
-  // If not a single input field is correct
-  if (Object.keys(validatedData).length === 0) {
-    throw new InvalidUpdateInput("No valid fields for update provided");
-  }
-
-  return validatedData;
 }
---
+++ src/utils/validation.utils.ts
@@ -0,0 +1,49 @@
+import { Festival } from "../entities/festival.entity";
+import { InvalidUpdateInput } from "../errors/custom-errors";
+import { FestivalUpdateDTO } from "../interfaces/festivalUpdateDTO.interface";
+
+// function validates the input data you receive and creates an Partial<Festival> object
+// every field is tested for typeof "string" and if the string only consists of whitespace characters
+// mandatory fields throw errors if they're empty
+export function validateFestivalUpdateInput(input: FestivalUpdateDTO): Partial<Festival> {
+  const validatedData: Partial<Festival> = {};
+
+  if (typeof input.name === "string" && input.name.trim() !== "") {
+    validatedData.name = input.name.trim();
+  } else if (input.name !== undefined) {
+    throw new InvalidUpdateInput("Invalid name provided");
+  }
+
+  if (typeof input.date === "string" && input.date.trim() !== "") {
+    // date-string is parsed to Date to validate correct ISO-8601 format
+    const parsedDate = Date.parse(input.date.trim());
+    if (isNaN(parsedDate)) {
+      throw new InvalidUpdateInput("Invalid ISO-8601 format for date field");
+    }
+
+    validatedData.date = input.date.trim();
+  } else if (input.date !== undefined) {
+    throw new InvalidUpdateInput("Invalid date provided (ISO-8601 format needed)");
+  }
+
+  if (typeof input.location === "string" && input.location.trim() !== "") {
+    validatedData.location = input.location.trim();
+  } else if (input.location !== undefined) {
+    throw new InvalidUpdateInput("Invalid location provided");
+  }
+
+  if (typeof input.description === "string") {
+    validatedData.description = input.description.trim();
+  }
+
+  if (typeof input.imageUrl === "string") {
+    validatedData.imageUrl = input.imageUrl.trim();
+  }
+
+  // If not a single input field is correct
+  if (Object.keys(validatedData).length === 0) {
+    throw new InvalidUpdateInput("No valid fields for update provided");
+  }
+
+  return validatedData;
+}
