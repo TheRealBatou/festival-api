b8bf7efc99dc2fe42f83202e9352c9bb22f2d2e6
Batou <m.schoepper@yahoo.de>
2025-05-09 23:58:10 +0200

    #18 created a integration test for the create festival api call

---
+++ src/__tests__/festival.create.test.ts
@@ -0,0 +1,44 @@
+// temporarily overwriting of DB_HOST for the test because the value in .env is set for docker (save the original value)
+//const originalHostEnv = process.env.DB_HOST;
+//process.env.DB_HOST = "localhost";
+
+import request from "supertest";
+import app from "../app";
+import { AppDataSource } from "../data-source";
+
+beforeAll(async () => {
+  // temporarily overwriting of DB_HOST for the test because the value in .env is set for docker
+  //process.env.DB_HOST = "localhost";
+
+  await AppDataSource.initialize();
+});
+
+afterAll(async () => {
+  // return the DB_HOST back to its original value
+  //process.env.DB_HOST = originalHostEnv;
+
+  await AppDataSource.destroy();
+});
+
+describe("creating a festival via POST API call", () => {
+  it("should create a new festival and return it", async () => {
+    const festivalData = {
+      name: "Test Festival",
+      date: "2026-07-07T10:00:00+02:00",
+      location: "Germany",
+      description: "Test Festival",
+      imageUrl: "test_festival.jpg",
+    };
+
+    const response = await request(app).post("/api/v1/festivals").send(festivalData).expect(201);
+
+    expect(response.body.name).toBe(festivalData.name);
+    expect(response.body.date).toBe(festivalData.date);
+    expect(response.body.location).toBe(festivalData.location);
+    expect(response.body.description).toBe(festivalData.description);
+    expect(response.body.imageUrl).toBe(festivalData.imageUrl);
+
+    expect(response.body.festivalId).toBeDefined();
+    expect(response.body.festivalId).not.toBeNull();
+  });
+});
