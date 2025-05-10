47dfc1d7de915a3a98de7aaa78e0f4b83eb8842b
Batou <m.schoepper@yahoo.de>
2025-05-10 14:57:42 +0200

    #21 created a logger with the Winston lib

---
+++ src/logger/logger.ts
@@ -0,0 +1,27 @@
+//Used lib for logging -> Winston
+import { createLogger, format, transports } from "winston";
+import fs from "fs";
+import path from "path";
+
+// makes sure, that the log folder is created if it doesn't exist yet
+const logDir = path.join(__dirname, "..", "logs");
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+// Creates a logger that writes into a log file "logs/app.log
+// Format: leading timestamp followed by the log-level and then the log-message
+// log-level info includes the log-levels: error, warn and info
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf(({ level, message, timestamp }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  // logs are written in the console and into a log-file
+  transports: [new transports.Console(), new transports.File({ filename: "logs/app.log" })],
+});
+
+export default logger;
